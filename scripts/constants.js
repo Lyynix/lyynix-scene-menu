61d35ff61c05e0fe9b13eaca22a4eef912cd2096
Fabian Lux <luxfabianl@gmail.com>
2024-12-16 15:25:27 +0100

    added functionality

---
+++ scripts/constants.js
@@ -0,0 +1,22 @@
+export const CONST = Object.freeze({
+  tileTooltipConfig: {
+    heading: "Kacheln",
+    src: "",
+    items: [
+      {
+        heading: "Umschalten",
+        reference: "CONTROLS.LeftClick"
+      }
+    ]
+  },
+  lightsTooltipConfig: {
+    heading: "Lichter",
+    src: "",
+    items: [
+      {
+        heading: "Umschalten",
+        reference: "CONTROLS.LeftClick"
+      }
+    ]
+  },
+})
